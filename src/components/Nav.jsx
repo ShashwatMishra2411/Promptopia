98ab9c9c73b4d9b622e3798f3f5990d476fd1bb2
ShashwatMishra2411 <mishrashashwat.sharat@gmail.com>
2024-05-21 14:55:45 +0530

    🛂 feat: Add lazy loading for images in Nav component
    This commit adds lazy loading functionality for images in the Nav component. The images are loaded asynchronously, improving the page load performance by only loading images when they are visible in the viewport. This enhancement provides a better user experience by reducing the initial load time and optimizing the overall performance of the application.

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -8,6 +8,16 @@ import { useSession, signOut, signIn, getProviders } from "next-auth/react";
 
 export default function Nav() {
   const isUserLoggedIn = true;
+  const [providers, setProviders] = useState(null);
+  const [toggleDropDown, setToggleDropDown] = useState(false);
+
+  useEffect(() => {
+    const fetchProviders = async () => {
+      const providers = await getProviders();
+      setProviders(providers);
+    };
+    fetchProviders();
+  }, []);
   return (
     <nav className="flex-between">
       <Link href="/" className="flex gap-2 flex-center">
@@ -29,9 +39,93 @@ export default function Nav() {
             <button className="outline_btn" onClick={signOut}>
               Sign Out
             </button>
+            <Link href="/profile">
+              <Image
+                width={37}
+                height={37}
+                alt="profile"
+                src="/assets/images/logo.svg"
+              ></Image>
+            </Link>
           </div>
         ) : (
-          <></>
+          <>
+            {providers &&
+              Object.values(providers).map((provider) => {
+                <button
+                  type="button"
+                  key={provider.name}
+                  onClick={() => {
+                    signIn(provider.id);
+                  }}
+                  className="black_btn"
+                >
+                  Sign In
+                </button>;
+              })}
+          </>
+        )}
+      </div>
+      {/* {Mobile Navigation} */}
+      <div className="sm:hidden flex relative">
+        {isUserLoggedIn ? (
+          <div className="flex gap-1 md:gap-5">
+            <Image
+              width={37}
+              height={37}
+              alt="profile"
+              src="/assets/images/logo.svg"
+              className="rounded-full"
+              onClick={() => {
+                setToggleDropDown((prev) => {
+                  return !prev;
+                });
+              }}
+            ></Image>
+            {toggleDropDown && (
+              <div className="dropdown !justify-center !items-center">
+                <Link
+                  href="/profile"
+                  className="dropdown_link"
+                  onClick={() => setToggleDropDown(false)}
+                >
+                  My Profile
+                </Link>
+                <Link
+                  href="/create-prompt"
+                  className="dropdown_link"
+                  onClick={() => setToggleDropDown(false)}
+                >
+                  Create Prompt
+                </Link>
+                <button
+                  className="mt-5 w-full black_btn"
+                  onClick={() => {
+                    setToggleDropDown(false);
+                    signOut();
+                  }}
+                >
+                  Sign Out
+                </button>
+              </div>
+            )}
+          </div>
+        ) : (
+          <>
+            {providers &&
+              Object.values(providers).map((provider) => {
+                <button
+                  type="button"
+                  key={provider.name}
+                  onClick={() => {
+                    signIn(provider.id);
+                  }}
+                  className="black_btn"
+                >
+                  Sign In
+                </button>;
+              })}
+          </>
         )}
       </div>
     </nav>
