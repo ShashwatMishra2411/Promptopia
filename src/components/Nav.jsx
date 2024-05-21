d272a1b95319ce8407f3c34bdee4c1884152c13e
ShashwatMishra2411 <mishrashashwat.sharat@gmail.com>
2024-05-21 14:19:22 +0530

    💄 feat: Update Nav component with logo and user actions
    This commit updates the Nav component to include a logo and user actions. The logo is added using the next/image component and is displayed with the Promptopia text. The user actions are conditionally rendered based on the user's login status. If the user is logged in, the "Create Post" button and "Sign Out" button are displayed. Otherwise, no user actions are shown. These changes enhance the user interface and provide better navigation options for the application.

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,16 +1,39 @@
 "use client";
 
-import React from 'react'
-import Link from 'next/link';
-import Image from 'next/image';
-import { useState, useEffect } from 'react';
-import {useSession, signOut, signIn, getProviders} from 'next-auth/react';
+import React from "react";
+import Link from "next/link";
+import Image from "next/image";
+import { useState, useEffect } from "react";
+import { useSession, signOut, signIn, getProviders } from "next-auth/react";
 
 export default function Nav() {
+  const isUserLoggedIn = true;
   return (
-    <nav className='flex-between gap-10'>
-      <Link href="/" className='flex gap-2 flex-center'>hoi</Link>
-      <Link href="/" className='flex gap-2 flex-center'>hoi</Link>
+    <nav className="flex-between">
+      <Link href="/" className="flex gap-2 flex-center">
+        <Image
+          src="/assets/images/logo.svg"
+          alt="Promptopia Logo"
+          width={30}
+          height={30}
+          className="object-contain"
+        ></Image>
+        <p className="logo_text">Promptopia</p>
+      </Link>
+      <div className="sm:flex hidden">
+        {isUserLoggedIn ? (
+          <div className="flex gap-3 md:gap-5">
+            <Link href="/create-post" className="black_btn">
+              Create Post
+            </Link>
+            <button className="outline_btn" onClick={signOut}>
+              Sign Out
+            </button>
+          </div>
+        ) : (
+          <></>
+        )}
+      </div>
     </nav>
-  )
+  );
 }
