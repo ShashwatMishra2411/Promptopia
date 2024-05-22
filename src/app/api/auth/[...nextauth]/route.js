c0fb40223d8b14ea9d6bd0b7acfdb8446a62c5ca
ShashwatMishra2411 <mishrashashwat.sharat@gmail.com>
2024-05-22 15:33:55 +0530

    🛂 feat: Add user model and database connection

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -1,6 +1,7 @@
 import NextAuth from "next-auth/next";
 import GoogleProvider from "next-auth/providers/google";
 import {connectToDB} from "@/utils/db";
+import User from "@/models/user";
 
 const handler = NextAuth({
   providers: [
@@ -12,11 +13,26 @@ const handler = NextAuth({
   async signIn({ profile }) {
     try {
       await connectToDB();
+      // check whether the user already exists in the database
+      const userExists = await User.findOne({email: profile.email});
+      // if not, create a new user
+      if(!userExists){
+        const newUser = new User({
+          email: profile.email,
+          username: profile.name.replace(" ", "").toLowerCase(),
+          image: profile.picture,
+        });
+        await newUser.save();
+      }
     } catch (error) {
       console.log(error);
     }
   },
-  async session({ session }) {},
+  async session({ session }) {
+    const sessionUser = await User.findOne({email: session.user.email});
+    session.user.id = sessionUser._id.toString();
+    return session;
+  },
 });
 
 export { handler as GET, handler as POST };
