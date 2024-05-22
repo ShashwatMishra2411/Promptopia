9731f8b422556cd0302c83f71cfaaaf7fa509ef0
ShashwatMishra2411 <mishrashashwat.sharat@gmail.com>
2024-05-22 15:34:12 +0530

    feat: Add image field to user model
    This commit adds an `image` field to the user model in the `user.js` file. The `image` field is of type `String` and allows users to store an image URL. This change enhances the functionality of the application by allowing users to have a profile picture associated with their account.

--- src/models/user.js
+++ src/models/user.js
@@ -12,8 +12,13 @@ const userSchema = new Schema({
     required: [true, "Username is required"],
     match: [/^[a-zA-Z0-9]+$/, "Username can only contain letters and numbers"],
   },
+  image:{
+    type: String,    
+  }
 });
 
 // models.User will be used if it is already defined, since this is a serverless function it will get called multiple times and create multiple schema, so to prevent that we are checking if the model is already defined or not through models.User and if its not defined then we are creating a new model through model("User", userSchema)
 
 const User = models.User || model("User", userSchema);
+
+export default User;
