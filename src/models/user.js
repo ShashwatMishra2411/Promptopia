33e1629549d3249829804f5339c2c332358b181e
ShashwatMishra2411 <mishrashashwat.sharat@gmail.com>
2024-05-22 11:27:17 +0530

    🗃️ feat: Add user model and database connection
    This commit adds a new user model to the project and establishes a database connection using the `connectToDB` function from the `db.js` module. The user model includes fields for email and username, with appropriate validation rules. The database connection is checked to prevent multiple schema creations. This change improves the functionality of the application by allowing user data to be stored and retrieved from the database.

---
+++ src/models/user.js
@@ -0,0 +1,19 @@
+import { match } from "assert";
+import { Schema, model, models } from "mongoose";
+
+const userSchema = new Schema({
+  email: {
+    type: String,
+    required: [true, "Email is required"],
+    unique: [true, "Email already exists"],
+  },
+  username: {
+    type: String,
+    required: [true, "Username is required"],
+    match: [/^[a-zA-Z0-9]+$/, "Username can only contain letters and numbers"],
+  },
+});
+
+// models.User will be used if it is already defined, since this is a serverless function it will get called multiple times and create multiple schema, so to prevent that we are checking if the model is already defined or not through models.User and if its not defined then we are creating a new model through model("User", userSchema)
+
+const User = models.User || model("User", userSchema);
