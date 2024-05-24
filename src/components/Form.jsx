6eff67eed8b8458d2225566ae4c4f8ec559c223d
ShashwatMishra2411 <mishrashashwat.sharat@gmail.com>
2024-05-25 01:22:20 +0530

    feat: Update form in Form component for better user experience
    The form in the Form component has been updated to improve the user experience. Changes include adding labels and placeholders for the prompt and tag fields, as well as styling enhancements for better readability. This update enhances the usability of the form and makes it more intuitive for users to create prompts.

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -17,10 +17,43 @@ export default function Form({
         {type} and share amazing prompts with the world, and let your
         imagination run wild with any AI-powered platform.
       </p>
-      <form action=""
-      onSubmit={handleSubmit}
-      className="mt-10 w-full max-w-2xl flex flex-col  gap-7 glassmorphism">
-        <label htmlFor=""><span className="font-satoshi font-semiold text-base text-gray-700">Your AI Prompt</span></label>
+      <form
+        action=""
+        onSubmit={handleSubmit}
+        className="mt-10 w-full max-w-2xl flex flex-col  gap-7 glassmorphism"
+      >
+        <label htmlFor="">
+          <span className="font-satoshi font-semiold text-base text-gray-700">
+            Your AI Prompt
+          </span>
+          <textarea
+            value={post.prompt}
+            onChange={(e) => setPost({ ...post, prompt: e.target.value })}
+            className="form_textarea"
+            placeholder="Write your prompt here..."
+            required
+          ></textarea>
+        </label>
+        <label htmlFor="">
+          <span className="font-satoshi font-semiold text-base text-gray-700">
+            Tag <span>(#product, #web, #k)</span>
+          </span>
+          <input
+            value={post.tag}
+            onChange={(e) => setPost({ ...post, tag: e.target.value })}
+            className="form_input"
+            placeholder="#tag"
+            required
+          ></input>
+        </label>
+        <div className="flex-end mx-3 mb-5 gap-4">
+          <Link href="/" className="text-gray-500 text-sm">
+            Cancel
+          </Link>
+          <button type="submit" className="px-5 py-1.5 text-sm bg-primary-orange rounded-full text-white" disabled={submitting}>
+            {submitting ? `${type}...` : type}
+          </button>
+        </div>
       </form>
     </section>
   );
