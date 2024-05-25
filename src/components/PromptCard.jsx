e5b2b4b81de30ea7de74801b01cd784c18bb4d01
ShashwatMishra2411 <mishrashashwat.sharat@gmail.com>
2024-05-26 03:13:43 +0530

    ✨ By using the `useSession` hook to access the user's session and the `usePathname` hook to get the current pathname, the component conditionally renders an edit and delete button only if the user is the creator of the prompt and the current page is the profile page. This enhancement improves the user experience by reducing the initial page load time and providing a more interactive interface for prompt management.

--- src/components/PromptCard.jsx
+++ src/components/PromptCard.jsx
@@ -9,8 +9,10 @@ export default function PromptCard({
   handleEdit,
   handleDelete,
 }) {
-  console.log("hello", post);
   const [copied, setCopy] = useState("");
+  const { data: session } = useSession();
+  const pathName = usePathname();
+  const router = useRouter();
   function handleCopy() {
     setCopy(post.prompt);
     navigator.clipboard.writeText(post.prompt);
@@ -59,6 +61,22 @@ export default function PromptCard({
         >
           #{post.tag}
         </p>
+        {session?.user.id === post.creator._id && pathName === "/profile" && (
+          <div className="mt-5 flex-center gap-4 border-t border-gray-100 pt-3">
+            <p
+              className="font-inter text-sm green_gradient cursor-pointer"
+              onClick={handleEdit}
+            >
+              Edit
+            </p>
+            <p
+              className="font-inter text-sm orange_gradient cursor-pointer"
+              onClick={handleDelete}
+            >
+              Delete
+            </p>
+          </div>
+        )}
       </div>
     </div>
   );
