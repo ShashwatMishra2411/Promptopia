47ebe9611bfb4bf5b9b6151f961c2bcffe94348e
ShashwatMishra2411 <mishrashashwat.sharat@gmail.com>
2024-05-25 22:12:54 +0530

    👽️ feat: Add GET API route for fetching prompts
    This commit adds a new API route, `GET /api/prompt`, for fetching prompts from the server. It connects to the database, retrieves all prompts, and populates the "creator" field. If successful, it returns a response with the fetched prompts and a status code of 200. If an error occurs, it logs the error message and returns a response with the error message and a status code of 500. This API route enhances the functionality of the application by providing a way to retrieve prompts for display.

---
+++ src/app/api/prompt/route.js
@@ -0,0 +1,14 @@
+import { connectToDB } from "@/utils/db";
+import Prompt from "@/models/prompt";
+
+export const GET = async (req, res) => {
+  try {
+    await connectToDB();
+    const prompts = await Prompt.find({}).populate("creator");
+    // console.log(prompts)
+    return new Response(JSON.stringify(prompts), { status: 200 });
+  } catch (e) {
+    console.log(e.message);
+    return new Response(e.message, { status: 500 });
+  }
+};
