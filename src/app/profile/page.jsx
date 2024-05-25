ee1f74f7a545173f0f18dba66cd19a5aed6571e8
ShashwatMishra2411 <mishrashashwat.sharat@gmail.com>
2024-05-25 22:13:22 +0530

    ✨ feat: Add Profile component for user profile page

---
+++ src/app/profile/page.jsx
@@ -0,0 +1,12 @@
+"use client";
+
+import React from 'react';
+import { useState, useEffect } from 'react';
+import { useSession } from 'next-auth/react';
+export default function Profile() {
+  return (
+    <div>
+      Profile
+    </div>
+  )
+}
