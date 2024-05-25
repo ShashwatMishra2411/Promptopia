64c67267d7a3a0c658b546655bd7866a72e0f658
ShashwatMishra2411 <mishrashashwat.sharat@gmail.com>
2024-05-26 03:12:32 +0530

    ✨ feat: Refactor profile page to use MyProfile component
    This commit refactors the profile page to use the newly created MyProfile component. The MyProfile component handles fetching the user's posts and rendering the Profile component with the appropriate data. This change improves code organization and separates concerns, making the profile page more maintainable and reusable.

--- src/app/profile/page.jsx
+++ src/app/profile/page.jsx
@@ -1,12 +1,38 @@
 "use client";
 
-import React from 'react';
-import { useState, useEffect } from 'react';
-import { useSession } from 'next-auth/react';
-export default function Profile() {
+import React from "react";
+import { useState, useEffect } from "react";
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
+
+import Profile from "@/components/Profile";
+
+export default function MyProfile() {
+  const { data: session } = useSession();
+  const [posts, setPosts] = useState([]);
+  const router = useRouter();
+  useEffect(() => {
+    // fetch data from the server
+    const fetchPosts = async () => {
+      const res = await fetch(`/api/users/${session?.user.id}/posts`);
+      const data = await res.json();
+      setPosts(data);
+    };
+    if (session?.user.id) fetchPosts();
+  }, [session]);
+  function handleEdit() {
+
+  }
+  function handleDelete() {}
   return (
     <div>
-      Profile
+      <Profile
+        name="My"
+        desc="welcome to your personalized profile page"
+        data={posts}
+        handleEdit={handleEdit}
+        handleDelete={handleDelete}
+      ></Profile>
     </div>
-  )
+  );
 }
