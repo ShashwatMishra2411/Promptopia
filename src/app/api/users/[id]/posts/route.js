f97c6e005ba7e9888c7aaf451d3ac3acb0bb2771
ShashwatMishra2411 <mishrashashwat.sharat@gmail.com>
2024-05-26 22:07:33 +0530

    🗑️ feat: Remove commented console.log statement in API route

--- src/app/api/users/[id]/posts/route.js
+++ src/app/api/users/[id]/posts/route.js
@@ -4,7 +4,7 @@ import Prompt from "@/models/prompt";
 export const GET = async (req, {params}) => {
   try {
     await connectToDB();
-    console.log(params)
+    // console.log(params)
     // console.log(id);
     const prompts = await Prompt.find({creator:params.id}).populate("creator");
     return new Response(JSON.stringify(prompts), { status: 200 });
