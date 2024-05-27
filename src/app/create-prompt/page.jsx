5dbd8bf9ca61565b8dead77b952b8615904adad3
ShashwatMishra2411 <mishrashashwat.sharat@gmail.com>
2024-05-27 22:47:54 +0530

    ✨ feat: Add GoogleGenerativeAI package for generating content
    This commit adds the "@google/generative-ai" package to the project, allowing the application to generate content using Google's generative AI models. The package is imported and used in the CreatePrompt component to generate content based on user input. This enhancement improves the functionality of the application by incorporating advanced AI capabilities for content generation.

--- src/app/create-prompt/page.jsx
+++ src/app/create-prompt/page.jsx
@@ -1,5 +1,7 @@
 "use client";
+const { GoogleGenerativeAI } = require("@google/generative-ai");
 
+// ...
 import React from "react";
 import { useState } from "react";
 import { useSession } from "next-auth/react";
@@ -7,18 +9,29 @@ import { useRouter } from "next/navigation";
 import Form from "@/components/Form";
 
 export default function CreatePrompt() {
-  const {data: session, status} = useSession();
+  const { data: session, status } = useSession();
   const [submitting, setSubmitting] = useState(false);
   const [post, setPost] = useState({
     prompt: "",
     tag: "",
   });
   const router = useRouter();
+  // Access your API key as an environment variable (see "Set up your API key" above)
+  const genAI = new GoogleGenerativeAI(process.env.NEXT_PUBLIC_GEMINI_API_KEY);
+  // ...
+
+  // The Gemini 1.5 models are versatile and work with both text-only and multimodal prompts
+  const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
   const createprompt = async (e) => {
     e.preventDefault();
     setSubmitting(true);
     try {
-      console.log("hillo",post);
+      // const prompt = "Write a story about a magic backpack."
+
+      const result = await model.generateContent(post.prompt);
+      const response = await result.response;
+      const text = response.text();
+      console.log(text);
       const res = await fetch("/api/prompt/new", {
         method: "POST",
         body: JSON.stringify({
@@ -29,11 +42,11 @@ export default function CreatePrompt() {
       });
       if (res.ok) {
         setPost({ prompt: "", tag: "" });
-        router.push("/");
+        // router.push("/");
       }
     } catch (e) {
-      console.log(e.message);
-    }finally{
+      alert(e.message);
+    } finally {
       setSubmitting(false);
     }
   };
