4348d81d0311ce934b7ec1556d1883c30b90a148
ShashwatMishra2411 <mishrashashwat.sharat@gmail.com>
2024-05-27 23:15:19 +0530

    ✨ feat: Add response field to POST API route
    This commit adds the "response" field to the POST API route in the route.js file. The "response" field is now included in the request body and stored in the database when creating a new prompt. This enhancement improves the functionality of the application by allowing users to provide a response along with their prompts.

--- src/app/api/prompt/new/route.js
+++ src/app/api/prompt/new/route.js
@@ -1,14 +1,15 @@
 import { connectToDB } from "../../../../utils/db";
 import Prompt from "../../../../models/prompt";
 export const POST = async (req, res) => {
-  const { prompt, tag, userId } = await req.json();
-  console.log(prompt, tag, userId)
+  const { prompt, tag, userId, response } = await req.json();
+  console.log(prompt, tag, userId, response)
   try {
     await connectToDB();
     const post = await Prompt.create({
       prompt: prompt,
       tag: tag,
       creator: userId,
+      response: response,
     });
     return new Response(JSON.stringify(post), { status: 201 });
   } catch (e) {
