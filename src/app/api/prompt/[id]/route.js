5ea2fb4adf5a5051ed80b9ce9a084c65e3f97b49
ShashwatMishra2411 <mishrashashwat.sharat@gmail.com>
2024-05-28 23:18:26 +0530

    ✨ feat: Add response field to PATCH API route

--- src/app/api/prompt/[id]/route.js
+++ src/app/api/prompt/[id]/route.js
@@ -19,13 +19,14 @@ export const GET = async (req, { params }) => {
 export const PATCH = async (req, { params }) => {
   try {
     await connectToDB();
-    const { prompt, tag } = await req.json();
+    const { prompt, tag, response } = await req.json();
     const existingPrompt = await Prompt.findById(params.id);
     if (!existingPrompt) {
       return new Response("Prompt not found", { status: 404 });
     }
     existingPrompt.prompt = prompt;
     existingPrompt.tag = tag;
+    existingPrompt.response = response;
     await existingPrompt.save();
     return new Response(JSON.stringify(existingPrompt), { status: 200 });
   } catch (e) {
