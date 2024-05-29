63394e9164fd75f156a494ab7112032c35008346
ShashwatMishra2411 <mishrashashwat.sharat@gmail.com>
2024-05-29 20:04:47 +0530

    feat: Enable logging of prompts in GET API route

--- src/app/api/prompt/route.js
+++ src/app/api/prompt/route.js
@@ -5,7 +5,7 @@ export const GET = async (req, res) => {
   try {
     await connectToDB();
     const prompts = await Prompt.find({}).populate("creator");
-    // console.log(prompts)
+    console.log(prompts)
     return new Response(JSON.stringify(prompts), { status: 200 });
   } catch (e) {
     console.log(e.message);
