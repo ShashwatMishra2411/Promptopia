1d6db63f871385c828baf4396930edb91d9c10b5
ShashwatMishra2411 <mishrashashwat.sharat@gmail.com>
2024-05-31 23:54:46 +0530

    feat: Improve page load performance with lazy loading for images
    This commit adds lazy loading functionality for images in the Feed component. By importing the "next/image" package and enabling lazy loading, the initial load time for the Feed component is reduced. Images will now be loaded only when they are visible on the screen, improving the overall page load performance.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,26 +2,21 @@
 
 import React from "react";
 import Feed from "@/components/Feed";
+
 export default function Home() {
   return (
     <div className="flex w-full flex-col justify-center items-center">
       <h1 className="text-center flex flex-col justify-center items-center head_text">
         Discover & Share
         <br className="max-md:hidden" />
-        <span className="orange_gradient text-center">AI-Powered Promots</span>
+        <span className="orange_gradient text-center">AI-Powered Prompts</span>
       </h1>
       <p className="desc text-center">
-        This app is a community-driven platform that leverages AI technology to
-        generate and share creative prompts. Users can input various prompts
-        into the application, which are then processed by the advanced AI model,
-        Gemini. The AI generates responses to these prompts, which are
-        subsequently posted to a public feed. The feed serves as a collaborative
-        space where users can view, engage with, and be inspired by the diverse
-        range of AI-powered responses. The platform aims to foster creativity,
-        knowledge sharing, and collaborative learning through the innovative use
-        of AI-generated content.
+        A community platform to generate and share creative prompts using AI.
+        Input prompts, see AI-generated responses, and engage with a collaborative feed
+        to inspire creativity and knowledge sharing.
       </p>
-      <Feed></Feed>
+      <Feed />
     </div>
   );
 }
