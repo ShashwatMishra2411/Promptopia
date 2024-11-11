7e6f51a4f2ed294b86edd83ecba9b8929e7ec78c
ShashwatMishra2411 <mishrashashwat.sharat@gmail.com>
2024-11-12 02:38:28 +0530

    Refactor user model to use mongoose and update schema definition

--- src/models/user.js
+++ src/models/user.js
@@ -1,23 +1,11 @@
-import { Schema, model, models } from "mongoose";
+import mongoose from 'mongoose';
 
-const userSchema = new Schema({
-  email: {
-    type: String,
-    required: [true, "Email is required"],
-    unique: [true, "Email already exists"],
-  },
-  username: {
-    type: String,
-    required: [true, "Username is required"],
-    match: [/^[a-zA-Z0-9]+$/, "Username can only contain letters and numbers"],
-  },
-  image:{
-    type: String,    
-  }
+const userSchema = new mongoose.Schema({
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  username: { type: String, required: true },
 });
 
-// models.User will be used if it is already defined, since this is a serverless function it will get called multiple times and create multiple schema, so to prevent that we are checking if the model is already defined or not through models.User and if its not defined then we are creating a new model through model("User", userSchema)
-
-const User = models.User || model("User", userSchema);
+const User = mongoose.models.User || mongoose.model('User', userSchema);
 
 export default User;
