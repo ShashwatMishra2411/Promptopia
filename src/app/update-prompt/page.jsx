73ad0b62adcc4f66f3273a3190e6cd507f08d119
ShashwatMishra2411 <mishrashashwat.sharat@gmail.com>
2024-11-12 02:38:00 +0530

    Refactor UpdatePromptComponent to fetch prompt details and update prompt

--- src/app/update-prompt/page.jsx
+++ src/app/update-prompt/page.jsx
@@ -1,16 +1,18 @@
 "use client";
 
 import React, { useEffect, useState, Suspense } from "react";
-import { useSession } from "next-auth/react";
+// import { useSession } from "next-auth/react";
 import { useSearchParams, useRouter } from "next/navigation";
 import Form from "@/components/Form";
 import { GoogleGenerativeAI } from "@google/generative-ai";
 
 function UpdatePromptComponent() {
   const searchParams = useSearchParams();
+  console.log(searchParams.get("id"));
   const router = useRouter();
-  const promptId = searchParams.get('id'); // Access the 'id' query parameter
-  const { data: session } = useSession();
+  const promptId = searchParams.get("id"); // Access the 'id' query parameter
+  // const { data: session } = useSession();
+  const [session, setSession] = useState(null);
   const [submitting, setSubmitting] = useState(false);
   const [post, setPost] = useState({
     prompt: "",
@@ -19,11 +21,23 @@ function UpdatePromptComponent() {
   });
 
   useEffect(() => {
+    const token = localStorage.getItem("token");
+    console.log("token = " + token);
+    if (token) {
+      const data = JSON.parse(token);
+      setSession({
+        user: { email: data.email.S, name: data.name.S },
+      });
+    }
     if (promptId) {
       const getPromptDetails = async () => {
         const res = await fetch(`/api/prompt/${promptId}`);
         const data = await res.json();
-        setPost({ prompt: data.prompt, tag: data.tag, response: data.response });
+        setPost({
+          prompt: data.prompt,
+          tag: data.tag,
+          response: data.response,
+        });
       };
 
       getPromptDetails();
@@ -33,7 +47,9 @@ function UpdatePromptComponent() {
   async function generatePrompt() {
     setSubmitting(true);
     try {
-      const genAI = new GoogleGenerativeAI(process.env.NEXT_PUBLIC_GEMINI_API_KEY);
+      const genAI = new GoogleGenerativeAI(
+        process.env.NEXT_PUBLIC_GEMINI_API_KEY
+      );
       const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
       const result = await model.generateContent(post.prompt);
       const response = await result.response;
@@ -49,6 +65,7 @@ function UpdatePromptComponent() {
   const updatePrompt = async (e) => {
     e.preventDefault();
     setSubmitting(true);
+    console.log(session);
     if (!promptId) return alert("Prompt not found");
 
     try {
@@ -58,7 +75,7 @@ function UpdatePromptComponent() {
           prompt: post.prompt,
           tag: post.tag,
           response: post.response,
-          userId: session.user.id,
+          userId: JSON.parse(localStorage.getItem("token")).email.S,
         }),
       });
 
