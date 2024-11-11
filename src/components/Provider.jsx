d5c9bf9a9ffdd3826630d6ee578d49573a6e6946
ShashwatMishra2411 <mishrashashwat.sharat@gmail.com>
2024-11-12 02:38:25 +0530

    Refactor Provider component to comment out unused imports and code

--- src/components/Provider.jsx
+++ src/components/Provider.jsx
@@ -1,11 +1,11 @@
-"use client";
-import React from 'react'
-import {SessionProvider} from 'next-auth/react';
+// "use client";
+// import React from 'react'
+// import {SessionProvider} from 'next-auth/react';
 
-export default function Provider({children, session}) {
-  return (
-    <SessionProvider session={session}>
-      {children}
-    </SessionProvider>
-  )
-}
+// export default function Provider({children, session}) {
+//   return (
+//     <SessionProvider session={session}>
+//       {children}
+//     </SessionProvider>
+//   )
+// }
