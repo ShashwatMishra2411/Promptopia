5abcd68973a6e6158d14b15947bf89d9557a181f
ShashwatMishra2411 <mishrashashwat.sharat@gmail.com>
2024-11-12 02:37:34 +0530

    Refactor prompt route to fetch all items from DynamoDB

--- src/app/api/prompt/[id]/route.js
+++ src/app/api/prompt/[id]/route.js
@@ -1,51 +1,96 @@
-import { connectToDB } from "@/utils/db";
-import Prompt from "@/models/prompt";
+import { NextResponse } from "next/server";
+import client from "@/utils/db";
+import {
+  GetItemCommand,
+  UpdateItemCommand,
+  DeleteItemCommand,
+} from "@aws-sdk/client-dynamodb";
+
 // GET (read)
 export const GET = async (req, { params }) => {
+  const { id } = params;
+  const param = {
+    TableName: "blog_info",
+    Key: {
+      blog_id: { S: id },
+    },
+  };
+
   try {
-    await connectToDB();
-    const prompt = await Prompt.findById(params.id).populate("creator");
-    if (!prompt) {
+    const command = new GetItemCommand(param);
+    const data = await client.send(command);
+    console.log(data);
+    const prompts = {
+      id: data.Item.blog_id.S,
+      prompt: data.Item.prompt.S,
+      tag: data.Item.tag.S,
+      creator: data.Item.creator.S,
+      response: data.Item.response.S,
+    };
+    if (!data.Item) {
       return new Response("Prompt not found", { status: 404 });
     }
-    return new Response(JSON.stringify(prompt), { status: 200 });
+    return new Response(JSON.stringify(prompts), { status: 200 });
   } catch (e) {
     console.log(e.message);
     return new Response(e.message, { status: 500 });
   }
 };
 
-// PATCH(update)
+// PATCH (update)
 export const PATCH = async (req, { params }) => {
+  const { id } = params;
+  const { prompt, tag, response } = await req.json();
+
+  const param = {
+    TableName: "blog_info",
+    Key: {
+      blog_id: { S: id },
+    },
+    UpdateExpression: "SET #p = :prompt, #t = :tag, #r = :response",
+    ExpressionAttributeNames: {
+      "#p": "prompt",
+      "#t": "tag",
+      "#r": "response",
+    },
+    ExpressionAttributeValues: {
+      ":prompt": { S: prompt },
+      ":tag": { S: tag },
+      ":response": { S: response },
+    },
+    ReturnValues: "ALL_NEW",
+  };
+
   try {
-    await connectToDB();
-    const { prompt, tag, response } = await req.json();
-    const existingPrompt = await Prompt.findById(params.id);
-    if (!existingPrompt) {
-      return new Response("Prompt not found", { status: 404 });
-    }
-    existingPrompt.prompt = prompt;
-    existingPrompt.tag = tag;
-    existingPrompt.response = response;
-    await existingPrompt.save();
-    return new Response(JSON.stringify(existingPrompt), { status: 200 });
+    const command = new UpdateItemCommand(param);
+    const data = await client.send(command);
+    return new Response(JSON.stringify(data.Attributes), { status: 200 });
   } catch (e) {
     console.log(e.message);
     return new Response(e.message, { status: 500 });
   }
 };
+
 // DELETE (delete)
-export const DELETE = async (req, { params }) =>{
-    try{
-        await connectToDB();
-        const prompt = await Prompt.findByIdAndDelete(params.id);
-        if(!prompt){
-            return new Response("Prompt not found", { status: 404 });
-        }
-        return new Response(JSON.stringify(prompt), { status: 200 });
-        
-    }catch(e){
-        console.log(e.message);
-        return new Response(e.message, { status: 500 });
+export const DELETE = async (req, { params }) => {
+  const { id } = params;
+  const param = {
+    TableName: "blog_info",
+    Key: {
+      blog_id: { S: id },
+    },
+    ReturnValues: "ALL_OLD",
+  };
+
+  try {
+    const command = new DeleteItemCommand(param);
+    const data = await client.send(command);
+    if (!data.Attributes) {
+      return new Response("Prompt not found", { status: 404 });
     }
-}
+    return new Response(JSON.stringify(data.Attributes), { status: 200 });
+  } catch (e) {
+    console.log(e.message);
+    return new Response(e.message, { status: 500 });
+  }
+};
