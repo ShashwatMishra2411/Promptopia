e26e28e83ca28f4ffacc07aaf4897ab56c2d3134
ShashwatMishra2411 <mishrashashwat.sharat@gmail.com>
2024-11-12 02:37:57 +0530

    Refactor profile page to fetch posts from server and handle post deletion

--- src/app/profile/page.jsx
+++ src/app/profile/page.jsx
@@ -2,34 +2,41 @@
 
 import React from "react";
 import { useState, useEffect } from "react";
-import { useSession } from "next-auth/react";
+// import { useSession } from "next-auth/react";
 import { useRouter } from "next/navigation";
 
 import Profile from "@/components/Profile";
 
 export default function MyProfile() {
-  const { data: session } = useSession();
+  // const { data: session } = useSession();
+  const [session, setSession] = useState(null);
   const [posts, setPosts] = useState([]);
   const router = useRouter();
   useEffect(() => {
+    const token = localStorage.getItem("token");
+    if (token) {
+      setSession(JSON.parse(token));
+    }
+    console.log(token);
     // fetch data from the server
     const fetchPosts = async () => {
-      const res = await fetch(`/api/users/${session?.user.id}/posts`);
+      const res = await fetch(`/api/users/${JSON.parse(token).email.S}/posts`);
       const data = await res.json();
       setPosts(data);
     };
-    if (session?.user.id) fetchPosts();
-  }, [session]);
+    // if (session?.user.id) fetchPosts();
+    fetchPosts();
+  }, []);
   function handleEdit(post) {
-    router.push(`/update-prompt?id=${post._id}`);
+    router.push(`/update-prompt?id=${post.id}`);
   }
   function handleDelete(post) {
     const hasConfirmed = confirm("Are you sure you want to delete this post?");
-    if(hasConfirmed){
-      fetch(`/api/prompt/${post._id}`, {
+    if (hasConfirmed) {
+      fetch(`/api/prompt/${post.id}`, {
         method: "DELETE",
       });
-      setPosts(posts.filter((p) => p._id !== post._id));
+      setPosts(posts.filter((p) => p.id !== post.id));
     }
   }
   return (
